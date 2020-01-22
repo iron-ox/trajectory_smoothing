69fb97c2f9e03a59b00c482513aef0c1c5842bd2
mahmoudali <m.a.a.m.ali@tudelft.nl>
2020-01-22 11:34:55 +0100

    rm fit_non_zero_seven_segment from seven_segment.pt

--- traj/src/traj/seven_segment.py
+++ traj/src/traj/seven_segment.py
@@ -2,7 +2,6 @@ from sympy import integrate, Symbol
 from sympy.core.numbers import Float
 
 from .piecewise_function import PiecewiseFunction
-#from planning_func import calculate_min_displacement_for_acc
 import traj
 
 def fit_seven_segment(p_start, p_end, v_max, a_max, j_max, independent_variable=Symbol('t')):
@@ -92,67 +91,3 @@ def fit_seven_segment(p_start, p_end, v_max, a_max, j_max, independent_variable=
 
 
 
-
-def fit_non_zero_seven_segment(p_start, p_end, v_start, v_end, v_max, a_max, j_max, independent_variable=Symbol('t')):
-    """
-    Find the optimal seven segment trajectory for zero start and end velocities, and the given
-    start and end positions.
-
-    Follows the nomenclature and approach of
-
-        Herrera-Aguilar, Ignacio, and Daniel Sidobre. "Soft motion trajectory planning and
-        control for service manipulator robot." Workshop on Physical Human-Robot Interaction in
-        Anthropic Domains at IROS. 2006.
-    """
-    assert(a_max > 0.0)
-    assert(j_max > 0.0)
-    assert(v_max > 0.0)
-
-#    T_j, T_a, T_v, acc, vel= traj.equal_vel_case_planning(p_end-p_start, v_start, v_max, a_max, j_max)
-#    print "T_j, T_a, T_v"
-#    print T_j, T_a, T_v
-    
-    t_jrk_to_vf, t_acc_to_vf, t_jrk, t_acc, t_vel = traj.non_zero_vel_segment_planning(p_start, p_end, v_start, v_end, v_max, a_max, j_max)
-    print "t_jrk_to_vf, t_acc_to_vf, t_jrk, t_acc, t_vel"
-    print t_jrk_to_vf, t_acc_to_vf, t_jrk, t_acc, t_vel
-    
-#    segment_jerks_and_durations = [(j_max, T_j), (0.0, T_a), (-j_max, T_j), (0.0, T_v), (-j_max,T_j), (0.0, T_a), (j_max, T_j)]
-    if v_end != v_start:
-         j_max_to_vf = ((v_end-v_start)/abs(v_end-v_start))*j_max
-    else:
-         j_max_to_vf=0
-    
-    
-    if v_end > v_start:
-        segment_jerks_and_durations = [(j_max_to_vf, t_jrk_to_vf), (0.0, t_acc_to_vf), (-j_max_to_vf, t_jrk_to_vf),    (j_max, t_jrk), (0.0, t_acc), (-j_max, t_jrk), (0.0, t_vel), (-j_max,t_jrk), (0.0, t_acc), (j_max, t_jrk)]
-    else:
-        segment_jerks_and_durations = [(j_max, t_jrk), (0.0, t_acc), (-j_max, t_jrk), (0.0, t_vel), (-j_max,t_jrk), (0.0, t_acc), (j_max, t_jrk),    (j_max_to_vf, t_jrk_to_vf), (0.0, t_acc_to_vf), (-j_max_to_vf, t_jrk_to_vf)]
-    
-    segments = []
-    p0 = p_start
-    v0 = v_start
-    a0 = 0.0
-    times = [0.0]
-    jerk_functions = []
-    acceleration_functions = []
-    velocity_functions = []
-    position_functions = []
-    # Integrate jerk starting from the start of the trajectory and going all the way through the end.
-    for j0, T in segment_jerks_and_durations:
-        times.append(times[-1] + T)
-        j = Float(j0)
-        a = integrate(j, independent_variable) + a0
-        v = integrate(a, independent_variable) + v0
-        p = integrate(v, independent_variable) + p0
-        jerk_functions.append(j)
-        acceleration_functions.append(a)
-        velocity_functions.append(v)
-        position_functions.append(p)
-        a0 = a.subs({independent_variable: T})
-        v0 = v.subs({independent_variable: T})
-        p0 = p.subs({independent_variable: T})
-    position = PiecewiseFunction(times, position_functions, independent_variable)
-    velocity = PiecewiseFunction(times, velocity_functions, independent_variable)
-    acceleration = PiecewiseFunction(times, acceleration_functions, independent_variable)
-    jerk = PiecewiseFunction(times, jerk_functions, independent_variable)
-    return position, velocity, acceleration, jerk

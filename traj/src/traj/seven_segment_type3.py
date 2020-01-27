9dc97363c6ad1532494398ea604750e283f0f69f
mahmoudali <m.a.a.m.ali@tudelft.nl>
2020-01-27 13:06:24 +0100

    add seven_segment_type3/4.py

--- traj/src/traj/seven_segment_type3.py
+++ traj/src/traj/seven_segment_type3.py
@@ -1,10 +1,10 @@
-from sympy import integrate, Symbol
+from sympy import integrate
 from sympy.core.numbers import Float
 
 from .piecewise_function import PiecewiseFunction
-import traj
 
-def fit_seven_segment(p_start, p_end, v_max, a_max, j_max, independent_variable=Symbol('t')):
+
+def fit(p_start, p_end, v_max, a_max, j_max, independent_variable):
     """
     Find the optimal seven segment trajectory for zero start and end velocities, and the given
     start and end positions.
@@ -15,9 +15,9 @@ def fit_seven_segment(p_start, p_end, v_max, a_max, j_max, independent_variable=
         control for service manipulator robot." Workshop on Physical Human-Robot Interaction in
         Anthropic Domains at IROS. 2006.
     """
-    assert(a_max > 0.0)
-    assert(j_max > 0.0)
-    assert(v_max > 0.0)
+    assert (a_max > 0.0)
+    assert (j_max > 0.0)
+    assert (v_max > 0.0)
 
     # Maximum amount of time we can spend at any of our limit conditions before we violate the
     # next higher limit condition.
@@ -31,15 +31,15 @@ def fit_seven_segment(p_start, p_end, v_max, a_max, j_max, independent_variable=
         # to the max velocity, and we adjust the max time spent in the max jerk limited
         # section to just reach the new max acceleration.
         T_amax = 0.0
-        T_jmax = (v_max / j_max)**0.5
+        T_jmax = (v_max / j_max) ** 0.5
         print(T_jmax, j_max)
         a_max = T_jmax * j_max
 
     # Compute the minimum distance that each case can travel. D_thr1 is the minimum distance for a
     # trajectory that hits both max acceleration and max velocity. D_thr2 is the minimum distance
     # for a trajectory that hits max acceleration but not max velocity.
-    D_thr1 = (a_max * v_max) / j_max + v_max**2 / a_max
-    D_thr2 = 2.0 * a_max**3 / j_max**2
+    D_thr1 = (a_max * v_max) / j_max + v_max ** 2 / a_max
+    D_thr2 = 2.0 * a_max ** 3 / j_max ** 2
 
     D = p_end - p_start
     if D >= D_thr1:
@@ -51,15 +51,15 @@ def fit_seven_segment(p_start, p_end, v_max, a_max, j_max, independent_variable=
         # We hit a_max but not v_max
         T_v = 0.0
         T_j = T_jmax
-        T_a = (a_max**2 / (4.0 * j_max) + D / a_max)**0.5 - 1.5 * a_max / j_max
+        T_a = (a_max ** 2 / (4.0 * j_max) + D / a_max) ** 0.5 - 1.5 * a_max / j_max
     else:
         # We hit neither a_max nor v_max
         T_v = 0.0
         T_a = 0.0
-        T_j = (D / (2.0 * j_max))**(1.0 / 3.0)
+        T_j = (D / (2.0 * j_max)) ** (1.0 / 3.0)
 
     segment_jerks_and_durations = [(j_max, T_j), (0.0, T_a), (-j_max, T_j), (0.0, T_v), (-j_max,
-        T_j), (0.0, T_a), (j_max, T_j)]
+                                                                                         T_j), (0.0, T_a), (j_max, T_j)]
     segments = []
     p0 = p_start
     v0 = 0.0
@@ -72,22 +72,7 @@ def fit_seven_segment(p_start, p_end, v_max, a_max, j_max, independent_variable=
     # Integrate jerk starting from the start of the trajectory and going all the way through the end.
     for j0, T in segment_jerks_and_durations:
         times.append(times[-1] + T)
-        j = Float(j0)
-        a = integrate(j, independent_variable) + a0
-        v = integrate(a, independent_variable) + v0
-        p = integrate(v, independent_variable) + p0
-        jerk_functions.append(j)
-        acceleration_functions.append(a)
-        velocity_functions.append(v)
-        position_functions.append(p)
-        a0 = a.subs({independent_variable: T})
-        v0 = v.subs({independent_variable: T})
-        p0 = p.subs({independent_variable: T})
-    position = PiecewiseFunction(times, position_functions, independent_variable)
-    velocity = PiecewiseFunction(times, velocity_functions, independent_variable)
-    acceleration = PiecewiseFunction(times, acceleration_functions, independent_variable)
+        jerk_functions.append(Float(j0))
+
     jerk = PiecewiseFunction(times, jerk_functions, independent_variable)
-    return position, velocity, acceleration, jerk
-
-
-
+    return jerk
